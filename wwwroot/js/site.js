5762784b77a97ff51167331206f341ef0a265dca
Fernando Daniel Hurtado Ribera <13669589@udivirtual.com.bo>
2026-05-30 01:48:44 -0400

    Agregar todas las vistas Razor y archivos estáticos

---
+++ wwwroot/js/site.js
@@ -0,0 +1,4 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static client files.
+
+// Write your JavaScript code.
